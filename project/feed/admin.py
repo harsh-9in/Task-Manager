8b50bfa9c23180e399b88a8bdd953dd0c05f6974
Harsh Kumar <harshkumar9.in@gmail.com>
2020-05-14 16:27:38 +0530

    bug fixes

--- project/feed/admin.py
+++ project/feed/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from feed.models import Feed
 # Register your models here.
+
+
+admin.site.register(Feed)
--- project/project/settings.py
+++ project/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5^vu=7o#y-+67l2!3btqu#)^d&-hwm@*hqurr6hb=kt#ig878m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
