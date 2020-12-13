277771f83755f98b31f6f447b0fa748675bf662c
Harsh Kumar <harshkumar9.in@gmail.com>
2020-12-13 23:02:41 +0530

    feat: show onlu user created feed

--- project/feed/permissions.py
+++ project/feed/permissions.py
@@ -1,13 +1,11 @@
 from rest_framework import permissions
-
+from feed.models import Feed
 
 
 
 class UpdateOwnStatus(permissions.BasePermission):
-
     def has_object_permission(self,request,view,obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
         return obj.user_profile.id==request.user.id
-        
