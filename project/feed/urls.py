51a0f8ff156a01b8d7c35311826a188093722a60
Harsh Kumar <54139847+harsh-9in@users.noreply.github.com>
2020-12-13 21:52:23 +0530

    refactor: Feed url (#5)

--- project/feed/urls.py
+++ project/feed/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from api import views
+from . import views
+
 
 
 router=DefaultRouter()
-router.register('signup',views.UserProfileViewset)
+router.register('feed',views.FeedViewset)
 
 
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('login/',views.UserLogin.as_view()),
 ]
