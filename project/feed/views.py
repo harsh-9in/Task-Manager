277d80eae7bb7e22194857f4bd35731fcec04818
Harsh Kumar <harshkumar9.in@gmail.com>
2020-12-13 23:09:06 +0530

    feat: show only user created feed

--- project/feed/views.py
+++ project/feed/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
-from feed import permissions
+from feed.permissions import UpdateOwnStatus
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from feed import serializers
-from feed import models
-from rest_framework.settings import api_settings 
+from feed.models import Feed
+from rest_framework.settings import api_settings
 
 
 
 class FeedViewset(viewsets.ModelViewSet):
     authentication_classes=(TokenAuthentication,)
     serializer_class=serializers.FeedSerializer
-    queryset=models.Feed.objects.all()
-    permission_classes=[permissions.UpdateOwnStatus , IsAuthenticated]
+    def get_queryset(self):
+        return Feed.objects.filter(user_profile=self.request.user)
+    permission_classes=[UpdateOwnStatus, IsAuthenticated]
     def perform_create(self,serializer):
         serializer.save(user_profile=self.request.user)
