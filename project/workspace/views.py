5c9728506f0a7bb1978fd8400934a37b9770539f
Harsh Kumar <54139847+harsh-9in@users.noreply.github.com>
2020-12-29 20:25:13 +0530

    Permissions for workspace (#12)

--- project/workspace/views.py
+++ project/workspace/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Board
 from . import serializers
+from rest_framework.permissions import IsAuthenticated
+from workspace.permissions import UpdateOwnStatus
 from feed.models import UserProfile
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
@@ -10,8 +12,10 @@ from rest_framework.authtoken.views import ObtainAuthToken
 
 class BoardViewsets(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
+    permission_classes=[IsAuthenticated,UpdateOwnStatus]
     serializer_class=serializers.BoardSerializer
-    queryset = Board.objects.all()
+    def get_queryset(self):
+        return Board.objects.filter(created_by=self.request.user)
     def perform_create(self, serializer):
         print(self.request.user)
         serializer.save(created_by=self.request.user)
