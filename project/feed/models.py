db0d054b173969fbb24aabc406cb76c1b70cd6e5
Harsh Kumar <harshkumar9.in@gmail.com>
2020-12-30 14:05:03 +0530

    feat: feed model connected to Borad

--- project/feed/models.py
+++ project/feed/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from  django.conf import settings
 from api.models import UserProfile
+from workspace.models import Board
 
 # Create your models here.
 
@@ -8,8 +9,12 @@ from api.models import UserProfile
 class Feed(models.Model):
       user_profile= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
       title=models.CharField(max_length=255)
-      text=models.TextField()
+      text=models.TextField(blank=True)
+      workspace=models.ForeignKey(Board,on_delete=models.CASCADE)
       created_on=models.DateTimeField(auto_now_add=True,auto_now=False)
 
       def __str__(self):
           return self.text
+
+
+
